647fe71bca0273bb7726b115066d3243f7ffb2a7
krishnachalise418 <krishnachalise418@gmail.com>
2019-06-22 20:56:17 +0545

    add dummy marksheet and about School fragments

--- app/src/main/java/com/example/marksheet/DashboardActivity.java
+++ app/src/main/java/com/example/marksheet/DashboardActivity.java
@@ -1,13 +1,17 @@
 package com.example.marksheet;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
     @Override
@@ -20,10 +24,35 @@ public class DashboardActivity extends AppCompatActivity {
 
         drawer = findViewById(R.id.drawer_layout);
 
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.open_navigation_drawer, R.string.close_navigation_drawer);
         drawer.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MarksheetFragment()).commit();
+            navigationView.setCheckedItem(R.id.marksheet);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.marksheet:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MarksheetFragment()).commit();
+                break;
+            case R.id.aboutschool:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AboutSchoolFragment()).commit();
+                break;
+            case R.id.feedback:
+                Toast.makeText(this, "Feedback Button", Toast.LENGTH_SHORT).show();
+            case R.id.aboutus:
+                Toast.makeText(this, "About App Button Clicked", Toast.LENGTH_SHORT).show();
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
